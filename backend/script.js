f8da11b2e6dbba95a360ca84b188603b0bc590c9
Muhammad-Taha-Hussain <zahoorqasim327@gmail.com>
2024-05-02 07:33:09 +0500

    second commit

--- backend/script.js
+++ backend/script.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.use(function (req, res, next) {
-    console.log("Number 360")
-    next()
-})
-app.get('/',async function(req, res) {
-    res.send("Hello World this is santosh")
-})
-app.get('/profile',async function(req, res) {
-    console.log(req)
-    res.send("Hello World this is santosh from profile")
-})
-app.listen(3010);
+// app.use(function (req, res, next) {
+//     console.log("Number 360")
+//     next()
+// })
+// app.get('/',async function(req, res) {
+//     res.send("Hello World this is santosh")
+// })
+// app.get('/profile',async function(req, res) {
+//     console.log(req)
+//     res.send("Hello World this is santosh from profile")
+// })
+// app.listen(3010);
